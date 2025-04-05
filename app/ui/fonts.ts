7d81e904986ee301c556a38931e8e3fd35e5ff31
sepehr//lester <sepehr.borji@yahoo.com>
2025-04-05 22:07:45 +0430

    توضیح تغییرات انجام شده

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
 import { Inter } from 'next/font/google';
- 
-export const inter = Inter({ subsets: ['latin'] });
+
+// مقداردهی صحیح فونت
+export const inter = Inter({ 
+  subsets: ['latin'], // 'latin' به جای 'inter'
+  variable: '--font-inter' // اختیاری - برای استفاده به عنوان متغیر CSS
+});
